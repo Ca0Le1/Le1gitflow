865da5207fb3e067e3fa021a438aed0c69de51ef
Ca0Le1 <810702970@qq.com>
2019-10-29 19:31:21 +0800

    init

---
+++ 1914admin/src/demo/less基本使用/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import  './index.less'
+function Less() {
+  return (
+    <div className="less">
+        <h3>lessDemo</h3>
+        <div className='son1'>
+
+        </div>
+        <div className='son2'>
+            
+        </div>
+    </div>
+  );
+} 
+
+export default Less;
